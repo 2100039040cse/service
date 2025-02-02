b6ebbbac6d5a195f5edbc16f82e353ebfe40e7e6
SK Sohail <124297825+2100039040cse@users.noreply.github.com>
2025-02-02 16:49:06 +0530

    Update urls.py

--- serapp/urls.py
+++ serapp/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("serlogin/", views.serlogin, name="serlogin"),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
