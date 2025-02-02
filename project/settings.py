93794267fe70a63d03adb275410129dcb3993047
SK Sohail <124297825+2100039040cse@users.noreply.github.com>
2025-02-03 00:15:45 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'serapp',
+    
 ]
 
 MIDDLEWARE = [
@@ -118,8 +118,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static'),
-    os.path.join(BASE_DIR, 'serapp/static'),
-    
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
