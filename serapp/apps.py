2a7ce3af9ba77e13f12e0c27d4b4687c723107c3
SK Sohail <124297825+2100039040cse@users.noreply.github.com>
2025-02-03 00:14:33 +0530

    Delete serapp directory

--- serapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SerappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'serapp'
--- serapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
- 
-def serlogin(request):
-    return render(request, "serapp/serlogin.html")
-# Create your views here.
